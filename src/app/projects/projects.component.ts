7763776638bfe7a1d15d9d64001fc98e01953d86
Uladzislava <santalova.lada25@gmail.com>
2024-11-15 12:36:41 +0100

    add

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -7,6 +7,7 @@ interface Project {
   desc: string;
   src: string;
   link: string;
+  stack: string[];
   serverLink?: string;
 }
 
@@ -21,21 +22,32 @@ export class ProjectsComponent {
   projects: Project[] = [
     {
       title: 'Chamomile Express',
-      desc: 'Train ticketing app. It was developed in a team of 2 people, using Angular framework and PrimeNG library.',
+      desc: 'Train ticket booking application with appropriate functionality, authorization features, and management of schedules, car types, stations and trips for administrative access.',
       src: 'train.png',
       link: 'https://train-app.netlify.app/',
+      stack: [
+        'Angular',
+        'RxJS',
+        'NgRx',
+        'PrimeNG',
+        'TypeScript',
+        'SCSS',
+        'Jest',
+      ],
     },
     {
       title: 'Echoes of vinyl',
-      desc: 'E-commerce app for selling vinyl records and record players. Developed in a team of 3 people. It was developed without using frameworks and libraries.',
+      desc: 'E-commerce app for selling vinyl records and record players. The "commercetools" platform was used as API. No third-party libraries were used.',
       src: 'e-commerce.png',
       link: 'https://echoes-of-vinyl.netlify.app/',
+      stack: ['TypeScript', 'SASS', 'Webpack', 'ESLint', 'Husky', 'Jest'],
     },
     {
       title: 'Youtube Client',
       desc: 'The first application developed using Angular framework. Basic features of the framework and Youtube API were used.',
       src: 'youtube.png',
       link: 'https://task7-youtube-client.netlify.app/',
+      stack: ['Angular', 'RxJS', 'NgRx', 'TypeScript', 'SCSS', 'Jest'],
     },
     {
       title: 'Async-Race',
@@ -43,6 +55,7 @@ export class ProjectsComponent {
       src: 'race.png',
       link: 'https://rolling-scopes-school.github.io/sunlaa-JSFE2023Q4/async-race/',
       serverLink: 'https://github.com/mikhama/async-race-api',
+      stack: ['TypeScript', 'CSS', 'Webpack', 'ESLint', 'Prettier'],
     },
     {
       title: 'Fun Chat',
@@ -50,30 +63,43 @@ export class ProjectsComponent {
       src: 'chat.png',
       link: 'https://rolling-scopes-school.github.io/sunlaa-JSFE2023Q4/fun-chat/',
       serverLink: 'https://github.com/rolling-scopes-school/fun-chat-server',
+      stack: [
+        'TypeScript',
+        'CSS',
+        'Webpack',
+        'Husky',
+        'ESLint',
+        'Prettier',
+        'WebSocket',
+      ],
     },
     {
       title: 'Puzzle',
       desc: 'A game for learning English by assembling pictures from puzzles. It supports progress tracking through the levels.',
       src: 'puzzle.png',
       link: 'https://rolling-scopes-school.github.io/sunlaa-JSFE2023Q4/rss-puzzle/',
+      stack: ['TypeScript', 'CSS', 'Webpack', 'Husky', 'ESLint', 'Prettier'],
     },
     {
       title: 'Nonograms',
       desc: 'A Nonograms game where you need to reveal the hidden picture using numerical clues. The game supports progress saving and a leaderboard.',
       src: 'nonogram.png',
       link: 'https://rolling-scopes-school.github.io/sunlaa-JSFE2023Q4/nonograms/',
+      stack: ['JavaScript', 'CSS'],
     },
     {
       title: 'Hangman',
       desc: 'A Hangman game where you need to guess the word based on a given clue, with a limited number of attempts.',
       src: 'hangman.png',
       link: 'https://rolling-scopes-school.github.io/sunlaa-JSFE2023Q4/hangman/',
+      stack: ['JavaScript', 'CSS'],
     },
     {
       title: 'Coffee house',
       desc: 'Layout created from Figma, with added interactivity using JavaScript.',
       src: 'coffee.png',
       link: 'https://rolling-scopes-school.github.io/sunlaa-JSFE2023Q4/coffee-house/home.html',
+      stack: ['HTML', 'CSS', 'JavaScript'],
     },
   ];
 }
